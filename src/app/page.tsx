42e7634618c697ba5797d75263ac83eef4d0bc69
hacker-flavio <theflavioherrera@gmail.com>
2025-04-06 18:39:11 -0700

    added message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
       <div style={{ fontSize: "24px" }}>Hello World</div>
       <div>
         This Website is currently in development and is having its dns records
-        switched over to vercel
+        switched over to vercel will port all code April 8th-April 9th from
+        github page cant wait to show :)
       </div>
     </div>
   );
