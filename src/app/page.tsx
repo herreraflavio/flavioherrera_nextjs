3f6046328d1b405ca93b24502cb23a2fa46eb274
herreraflavio <lurker@flavioherrera.com>
2025-07-13 15:32:41 -0700

    small edit to personal website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,8 @@ export default function Home() {
   return (
     <div>
       <div style={{ fontSize: "24px" }}>Hello World</div>
-      <div>
-        This Website is currently in development and is having its dns records
-        switched over to vercel will port all code maybe by summer time from
-        github page cant wait to show :)
-      </div>
-      <b>
-        i dunno i wish i had more time, anyways yes i can code, it might not
-        look like it but trust, you are looking at a 10x developer 😎
-      </b>
+      <div>This Website is currently in development</div>
+
       <ImageTransformPlayground />
     </div>
   );
